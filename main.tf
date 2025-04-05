658422a815066e7da5044e248e7a4523ea0d8014
abhijitbehera304 <abhijitbehera304@gmail.com>
2025-04-05 14:26:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ count = 4
 ami = "ami-02f624c08a83ca16f"
 instance_type ="t2.medium"
 key_name = "Unixserver"
-vpc_security_groups_ids = ["sg-06532f4ed9cad6107"]
+vpc_security_group_ids = ["sg-06532f4ed9cad6107"]
 tags = {
 Name = var.instance_names[count.index]
 }
