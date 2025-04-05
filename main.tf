368dff6818538045ec35d2c1b79fdd7a3b6fe090
abhijitbehera304 <abhijitbehera304@gmail.com>
2025-04-05 15:02:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ resource "aws_instance" "one" {
 count = 4
 ami = "ami-04aa00acb1165b32a"
 instance_type = "t2.medium"
-key_name = "Unixserver"
-vpc_security_group_ids = ["sg-09a3b24eee1e5d4b9"]
+key_name = "Omm"
+vpc_security_group_ids = ["sg-0e2fec5489f24e1a2"]
 tags = {
 Name = var.instance_names[count.index]
 }
