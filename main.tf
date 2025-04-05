a88debed9349a5faccd68fe5466d491c8bb269af
abhijitbehera304 <abhijitbehera304@gmail.com>
2025-04-05 14:36:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ region = "us-east-1"
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-02f624c08a83ca16f"
-instance_type ="t2.medium"
+ami = "ami-04aa00acb1165b32a"
+instance_type = "t2.medium"
 key_name = "Unixserver"
 vpc_security_group_ids = ["sg-06532f4ed9cad6107"]
 tags = {
