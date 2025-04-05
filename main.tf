d11aeb85d46803205816e631374ece71774d8e9e
abhijitbehera304 <abhijitbehera304@gmail.com>
2025-04-05 14:56:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,12 +7,8 @@ count = 4
 ami = "ami-04aa00acb1165b32a"
 instance_type = "t2.medium"
 key_name = "Unixserver"
-vpc_security_group_ids = ["sg-06532f4ed9cad6107"]
+vpc_security_group_ids = ["sg-09a3b24eee1e5d4b9"]
 tags = {
 Name = var.instance_names[count.index]
 }
-}
-
-variable "instance_names" {
-default = ["jenkins", "tomcat-1", "tomcat-2", "Monitoring server"]
 }
